f8b1f25edf31d66ff1546052e8c34aaf31ed6d8e
Ashraful-89 <97118095+Ashraful-89@users.noreply.github.com>
2022-06-21 18:22:38 +0600

    Constant program added for use of strings

---
+++ Constant.java
@@ -0,0 +1,14 @@
+public class Constant {
+    public static final String StudentList= "students.txt";
+    public static final String showAll = "a";
+    public static final String showAl = "r";
+    public static final String showAlll = "c";
+
+
+    public static final String loadingData = "Loading Data...";
+    public static final String loadedData = "Loaded Data...";
+
+
+    public static final String addOperator = "+";
+    public static final String questionMark = "?";
+}
